8bd8a039ced631a45f1b8344c7e643215cfcd8ef
Alan Hawse <engineer@iotexpert.com>
2017-06-20 16:34:14 -0400

    Updates to I2C Project

--- 7-Shared-I2C.cydsn/main.c
+++ 7-Shared-I2C.cydsn/main.c
@@ -24,6 +24,7 @@ typedef enum Color_t {
 QueueHandle_t colorQueue;
 SemaphoreHandle_t i2cSemaphore;
 SemaphoreHandle_t countingSemaphore;
+TaskHandle_t accelHandle;
 
 void setupFreeRTOS()
 {
@@ -63,13 +64,81 @@ void LED_Task(void *arg)
 	}
 }
 
+CY_PACKED typedef struct accel {
+    int16_t x;
+    int16_t y;
+    int16_t z;
+} CY_PACKED_ATTR accel_t;
+
+
+// This task manages the aceleromter
+// 
+void accel_Task(void *arg)
+{
+    (void)arg;
+    SemaphoreHandle_t mySemaphore;
+    i2cm_transaction_t mytransaction;
+    char buff[128];
+    int x,y,z;
+    int xs,ys,zs;
+    
+    int byteCount;
+    accel_t myData;
+    
+    mySemaphore = xSemaphoreCreateBinary();
+     
+    // True of all read/writes
+    mytransaction.i2cm_address = 0x0F;
+    mytransaction.i2cm_regType = I2CM_BIT8;
+    mytransaction.i2cm_bytesProcessed = &byteCount;
+    
+    // Initalize the acceleromter
+    uint8_t setupData[] = {0b10000000};
+    mytransaction.i2cm_method = I2CM_WRITE;
+    mytransaction.i2cm_register = 0x1b; // CTRL_REG_1
+    mytransaction.i2cm_byteNum = sizeof(setupData);
+    mytransaction.i2cm_doneSemaphore = mySemaphore;
+    mytransaction.i2cm_bytes = setupData;
+    i2cm_runTransaction(&mytransaction);
+    
+    
+    // Setup for repeated reads
+    mytransaction.i2cm_method = I2CM_READ;
+    mytransaction.i2cm_register = 0x06;
+    mytransaction.i2cm_byteNum = sizeof(myData);
+    mytransaction.i2cm_doneSemaphore = mySemaphore;
+    mytransaction.i2cm_bytes = (uint8_t *)&myData;
+        
+    while(1)  // Run the same transaction over and over
+    {
+        i2cm_runTransaction(&mytransaction);
+        myData.x /= 16;  // The KXTJ2 Datasheet say lower 4 bits are X
+        myData.y /= 16;  // The KXTJ2 Datasheet say lower 4 bits are X
+        myData.z /= 16;  // The KXTJ2 Datasheet say lower 4 bits are X
+        xs = (myData.x < 0? -1:1);  // Find the XSign
+        ys = (myData.y < 0? -1:1);  // Find the YSign
+        zs = (myData.z < 0? -1:1);  // Find the ZSign
+            
+        // The 1024 is a hardcode based on the range of the acceleromter
+        x = (int)((float)myData.x / 1024.0 * 10.0) * xs; // Turn it into integer G * 10
+        y = (int)((float)myData.y / 1024.0 * 10.0) * ys; // Turn it into integer G * 10
+        z = (int)((float)myData.z / 1024.0 * 10.0) * zs; // Turn it into integer G * 10
+       
+        // Format data in x.x  
+        sprintf(buff,"x=%s%d.%dg\ty=%s%d.%dg\tz=%s%d.%dg\n",(xs<0?"-":""),x/10,x%10,(ys<0?"-":""),y/10,y%10,(zs<0?"-":""),z/10,z%10);
+        UART_UartPutString(buff);
+        vTaskDelay(1000); // delay for 1 second
+    }
+}
+
 void UART_Task(void *arg)
 {
     (void)arg;
     static char buff[500];
     uint8_t data[10];
     UART_Start();
-    UART_UartPutString("Started UART\n\r");
+    UART_UartPutString("Started Project\n");
+    UART_UartPutString("Press ? for help\n");
     char c;
     Color_t tempColor;
     int byteCount;
@@ -116,7 +185,6 @@ void UART_Task(void *arg)
                     // This will read 4 bytes from registers 0,1,2,3
                     // then print them on the terminal
             case 'q':
-                    
                     mytransaction.i2cm_address = 0x50;
                     mytransaction.i2cm_regType = I2CM_BIT16;
                     mytransaction.i2cm_byteNum = 4;
@@ -129,8 +197,7 @@ void UART_Task(void *arg)
                     sprintf(buff,"Bytes=%d 0=%d 1=%d 2=%d 3=%d\n",byteCount,data[0],data[1],data[2],data[3]);
                     UART_UartPutString(buff);                  
             break;
-            case 'i':
-                    // This will increment the values (that you read presumably from the FRAM
+            case 'i': // This will increment the values (that you read presumably from the FRAM
                     data[0] += 1;
                     data[1] += 1;
                     data[2] += 1;
@@ -139,9 +206,7 @@ void UART_Task(void *arg)
                     UART_UartPutString(buff);
             break;
            
-            case 'w':
-        
-                    // This will write 4 values to register 0 in the FRAM
+            case 'w': //This will write 4 values to register 0 in the FRAM
                     mytransaction.i2cm_address = 0x50;
                     mytransaction.i2cm_regType = I2CM_BIT16;
                     mytransaction.i2cm_byteNum = 4;
@@ -154,7 +219,32 @@ void UART_Task(void *arg)
                     sprintf(buff,"Wrote %d bytes\n",byteCount);
                     UART_UartPutString(buff);
             break;
-        }
+                    
+            case 's': // stop the accel task 
+                    vTaskSuspend(accelHandle);
+            break;
+                    
+            case 'S':  // start the accel task back up
+                    vTaskResume(accelHandle);
+            break;
+            case '?':
+                    UART_UartPutString(" ----------------------------\n");
+                    UART_UartPutString(" Help Commands\n");
+                    UART_UartPutString(" ----------------------------\n");
+                    
+                    UART_UartPutString("r-change the led color to red\n");
+                    UART_UartPutString("g-change the led color to green\n");
+                    UART_UartPutString("b-change the led color to blue\n");
+                    UART_UartPutString("t-dump the FreeRTOS Task List\n");
+                    UART_UartPutString("q-Read 4 values from the FRAM into data[0-3]\n");
+                    UART_UartPutString("i-Increment the values in data[0-3] by 1\n");
+                    UART_UartPutString("w-Write 4 values from data[0-3] to the FRAM at location 0 \n");
+                    UART_UartPutString("s-Stop Acceleromter Task\n");
+                    UART_UartPutString("S-Start Acceleromter Task\n");
+                    UART_UartPutString("?-List the commands\n");
+                    break;
+           }
+        
         taskYIELD();
     }
 }
@@ -181,7 +271,7 @@ int main(void)
     xTaskCreate(
         UART_Task,       /* Task function */
         "UART",          /* Task name (string) */
-        0x400,           /* Task stack, allocated from heap */
+        750,           /* Task stack, allocated from heap */
         0,               /* No param passed to task function */
         1,               /* Low priority */
         0 );             /* Not using the task handle */
@@ -195,6 +285,16 @@ int main(void)
         (void *)5,       /* Size of the queue */
         1,               /* Low priority */
         0 );
+    
+    xTaskCreate(
+        accel_Task,     /* Task function */
+        "Accel Master",   /* Task name (string) */
+        0x200,           /* Task stack, allocated from heap */
+        0,               /* No Argument */
+        1,               /* Low priority */
+        &accelHandle );
+    
+    vTaskSuspend(accelHandle); // Start with the acceleromter task suspended
     
     vTaskStartScheduler();
     while(1);
